0819cd134a05b7828ebc1d0a9e407bc58606f1a0
estherwee <estherweexueli@gmail.com>
2022-06-16 01:31:41 +0800

    Update users.js

--- models/users.js
+++ models/users.js
@@ -6,7 +6,6 @@
     email: { type: String, required: true, unique: true },
     firstname: { type: String, required: true },
     lastname: { type: String, required: true }
-
- })
+ });
 
  module.exports = mongoose.model("Users", usersSchema)
