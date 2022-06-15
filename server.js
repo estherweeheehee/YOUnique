b4c4b9964cbe9f06aba58e13de950ae119f0f3ba
estherwee <estherweexueli@gmail.com>
2022-06-16 01:08:27 +0800

    updated server, usersController and users.js

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const usersController = require("./controllers/UsersController");
 
 const app = express();
 const port = process.env.PORT ?? 3000;
+const MONGO_URI = process.env.MONGO_URI ?? "mongodb://localhost:27017/users" 
 
+mongoose.connection.on("error", (err) =>
+  console.log(err.message + " is Mongod not running?")
+);
+mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
+
+mongoose.connect(MONGO_URI);
+mongoose.connection.once("open", () => {
+  console.log("connected to mongoose...");
+});
+
+app.use(express.json());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    // cookie: { secure: true }
+  }))
 app.use(express.static("./frontend/dist"))
+app.use("/api/users", usersController);
 
 app.get("/api", (req, res) => {
     res.send("hello world")
