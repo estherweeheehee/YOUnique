1fccc0aea0e0fb526a807ec04785716a03ef0989
estherwee <estherweexueli@gmail.com>
2022-06-17 11:12:39 +0800

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import './App.css'
+import "./App.css";
 import Sell from "./pages/Sell";
 import Feed from "./pages/Feed";
 import Login from "./pages/Login";
@@ -11,20 +11,17 @@ import Main from "./pages/Main";
 import Search from "./pages/Search";
 
 function App() {
-  
-
   return (
     <div className="App">
       <BrowserRouter>
-        
         <Routes>
           <Route path={"/"} element={<Main />}>
-          <Route path={"/"} element={<Home />} />
-          <Route path={"/sell"} element={<Sell />} />
-          <Route path={"/feed"} element={<Feed />} />
-          <Route path={"/login"} element={<Login />} />
-          <Route path={"/post"} element={<Post />} />
-          <Route path={"/search/:id"} element={<Search />} />
+            <Route path={"/"} element={<Home />} />
+            <Route path={"/sell"} element={<Sell />} />
+            <Route path={"/feed"} element={<Feed />} />
+            <Route path={"/login"} element={<Login />} />
+            <Route path={"/post"} element={<Post />} />
+            <Route path={"/search/:id"} element={<Search />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,20 +1,35 @@
 const Login = () => {
-    const handleSubmit = () => {
-        
-    }
+  const handleSubmitLogin = () => {};
+
+  const handleSubmitSignup = () => {
+
+  }
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <fieldset>
-          <legend>Login</legend>
-          <label htmlFor="username">Username</label>
-          <input required name="username" id="username" />
-          <label htmlFor="password">Password</label>
-          <input name="password" id="password" />
-          <button>Login</button>
-        </fieldset>
-      </form>
+      <div>
+        <div className="login">
+          <form onSubmit={handleSubmitLogin}>
+            <fieldset>
+              <legend>Login</legend>
+              <label htmlFor="username">Username</label>
+              <input required name="username" id="username" />
+              <label htmlFor="password">Password</label>
+              <input name="password" id="password" />
+              <button>Login</button>
+            </fieldset>
+          </form>
+        </div>
+
+        <div className="signup">
+          <form onSubmit={handleSubmitSignup}>
+            <fieldset>
+              
+            </fieldset>
+
+          </form>
+        </div>
+      </div>
     </>
   );
 };
