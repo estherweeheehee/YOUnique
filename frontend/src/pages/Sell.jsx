4d007d9c6088b1acbad2986f273b9f5597ed2cb7
limjiehao <jiehao15@gmail.com>
2022-06-20 11:21:28 +0800

    updated sell

--- frontend/src/pages/Sell.jsx
+++ frontend/src/pages/Sell.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import { useAtom } from "jotai";
 import { userAtom } from "../App";
+import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
 import ImgAndDesc from "../components/ImgAndDesc";
 
 
 function Sell() {
     const [user, setUser] = useAtom(userAtom);    
-    const { id } = useParams();
-    const [product, setProduct] = useState("")
-    // const [state,setState] = useState({})
-    //? Fetch
+    const [product, setProduct] = useState([])
+    
+    // initial Fetch
     useEffect(() => {
-      fetch(`/api/feed/user/${id}`)
+      fetch(`/api/product/user/${user._id}`)
         .then((response) => response.json())
-        .then((data) => setProduct(data));
+        .then((data) => setProduct(data))
     }, []);
+    
+
 
     //   const handleEdit = (event)=>{
     //     const id = {
@@ -49,16 +50,24 @@ function Sell() {
     //   }
 
   return (
-    <>
+    <div>
+      <h1>Esther please insert user profile img as a map</h1>
       <div className="container">
         <div className="leftColumn">
         <ImgAndDesc />
         </div>
         <div className="rightColumn">
-          
-          <h1>
-            Multiple items
-          </h1>
+        <div>
+          {product.map((product) => (
+            <div key={product._id}>
+              <Link to={"/product/" + product._id}>
+                <img src={product.product_image} alt="" />
+              </Link>
+              <p>{product.product_name}</p>
+              <p>{product.product_listed_date}</p>
+            </div>
+          ))}
+        </div>
         </div>
 
       </div>
@@ -77,7 +86,7 @@ function Sell() {
       })} */}
       
 
-    </>
+    </div>
   );
 }
 
