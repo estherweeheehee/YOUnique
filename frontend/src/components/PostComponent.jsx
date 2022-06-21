6da8227423c428143dac71b13a78e02ea9191d58
estherwee <estherweexueli@gmail.com>
2022-06-21 10:30:44 +0800

    Update PostComponent.jsx

--- frontend/src/components/PostComponent.jsx
+++ frontend/src/components/PostComponent.jsx
@@ -5,18 +5,32 @@ import {useState} from 'react'
 
 function Post({setPost, post}) {
   const [user, setUser] = useAtom(userAtom);    
-  const [newPost, setNewPost] = useState([])
+  const [newPost, setNewPost] = useState({
+    post: "",
+    Image_url: "",
+    userid: user._id
+  })
+
+  const handleChange = (event, catagory) => {
+    setNewPost({
+      ...newPost,
+      [catagory]: event.target.value
+    })
+  }
 
   const handleSubmit = (event) => {
     
     event.preventDefault();
     fetch("/api/feed", {
       method: "POST",
-      body: JSON.stringify({
-        post: event.target.post.value,
-        Image_url: event.target.image.value,
-        userid: user._id
-      }),
+      body: JSON.stringify(
+        newPost
+      //   {
+      //   post: event.target.post.value,
+      //   Image_url: event.target.image.value,
+      //   userid: user._id
+      // }
+      ),
       headers: {
         "Content-Type": "application/json",
       },
@@ -24,8 +38,15 @@ function Post({setPost, post}) {
       .then((res) => res.json())
       .then((data) => {
         setPost([data, ...post])
+        setNewPost({
+          ...newPost,
+          post: "",
+         Image_url: ""
+        })
       })
       .catch((error) => console.error({ Error: error }));
+
+
       
   };
   return (
@@ -34,9 +55,9 @@ function Post({setPost, post}) {
         <fieldset>
           <legend>Submit a post</legend>
           <label htmlFor="post">Post</label>
-          <input required name="post" id="post" />
+          <input required name="post" id="post" value={newPost.post} onChange={() => handleChange(event, "post")}/>
           <label htmlFor="image">Image</label>
-          <input name="image" id="image" />
+          <input name="image" id="image" value={newPost.Image_url} onChange={() => handleChange(event, "Image_url")}/>
           <button>Upload post</button>
         </fieldset>
       </form>
