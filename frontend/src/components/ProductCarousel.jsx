be71f3718647df793bafd2ece1cec652897a7835
BigBirdGamer <87754100+BigBirdGamer@users.noreply.github.com>
2022-06-21 15:00:06 +0800

    idk

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,5 +1,32 @@
-const ProductCarousel = () => {
+import React from "react";
+import { Splide, SplideTrack, SplideSlide } from "@splidejs/react-splide";
+import { useEffect, useState } from "react";
+import "@splidejs/react-splide/css/core";
 
+function ProductCarousel() {
+  const [product, setProduct] = useState([]);
+  useEffect(() => {
+    fetch(`/api/product`)
+      .then((response) => response.json())
+      .then((data) => setProduct(data));
+  }, []);
+
+  return (
+    <div>
+      <h3>Products</h3>
+
+      {product.map((para) => {
+        return (
+          <div key={para._id}>
+        
+                  <p>{para.product_name}</p>
+                  <img src={para.product_image} alt="" />
+             
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default ProductCarousel
+export default ProductCarousel;
