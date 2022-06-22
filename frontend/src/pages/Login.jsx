da4969f26a216e2972a1688ad4dddf4c15580f91
limjiehao <jiehao15@gmail.com>
2022-06-22 17:43:45 +0800

    login screen css updated.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,7 +4,6 @@ import { userAtom } from "../App";
 import { atomWithStorage, RESET } from 'jotai/utils'
 import { useNavigate } from "react-router-dom";
 
-
 const Login = () => {
   const [user, setUser] = useAtom(userAtom);
   
@@ -37,36 +36,106 @@ const Login = () => {
     };
         
     return (
-      <div>
-        <form method="post" onSubmit={handleSubmitLogin}>
-            <fieldset>
-              <legend>Login</legend>
-              <label htmlFor="username">Username</label>
-              <input 
-              className="inputfield" 
-              required 
-              type="text"
-              placeholder="username"
-              name="username" 
-              id="username" 
-              value={login.username}
-              onChange={() => handleChange(event, "username")}
-              />
+<>
+      {/*
+        This example requires updating your template:
 
-              <label htmlFor="password">Password</label>
-              <input className="inputfield" 
-              required 
-              type="text"
-              placeholder="password"
-              name="password" 
-              id="password" 
-              value={login.password}
-              onChange={() => handleChange(event, "password")}
-              />
-              <button>Login</button>
-            </fieldset>
-          </form>
+        ```
+        <html class="h-full bg-gray-50">
+        <body class="h-full">
+        ```
+      */}
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        {/* logo */}
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <img
+              className="mx-auto h-12 w-auto"
+              src="https://i.imgur.com/4fZaiaq.png"
+              alt="Workflow"
+            />
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
           </div>
+
+          {/* email form */}
+          <form className="mt-8 space-y-6" action="#" method="POST" onSubmit={handleSubmitLogin}>
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="username" className="sr-only">Username</label>
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  autoComplete="username"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Username"
+                  value={login.username}
+                  onChange={() => handleChange(event, "username")}
+                />
+              </div>
+
+              {/* password form */}
+              <div>
+                <label htmlFor="password" className="sr-only">Password</label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                  value={login.password}
+                  onChange={() => handleChange(event, "password")}
+                />
+              </div>
+            </div>
+
+            {/* button */}
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+      // <div>
+      //   <form method="post" onSubmit={handleSubmitLogin}>
+      //       <fieldset>
+      //         <legend>Login</legend>
+      //         <label htmlFor="username">Username</label>
+      //         <input 
+      //         className="inputfield" 
+      //         required 
+      //         type="text"
+      //         placeholder="username"
+      //         name="username" 
+      //         id="username" 
+      //         value={login.username}
+      //         onChange={() => handleChange(event, "username")}
+      //         />
+
+      //         <label htmlFor="password">Password</label>
+      //         <input className="inputfield" 
+      //         required 
+      //         type="text"
+      //         placeholder="password"
+      //         name="password" 
+      //         id="password" 
+      //         value={login.password}
+      //         onChange={() => handleChange(event, "password")}
+      //         />
+      //         <button>Login</button>
+      //       </fieldset>
+      //     </form>
+      //     </div>
     )
 }
 
