d27f21539345cb55aca3c0a5febebdf3b6104c5b
limjiehao <jiehao15@gmail.com>
2022-06-22 13:49:16 +0800

    NavBar completed.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -6,7 +6,6 @@ import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { SearchIcon } from '@heroicons/react/solid'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
-import { PlusSmIcon } from '@heroicons/react/solid'
 
 const navigation = [
   { name: "Home", to: "/"},
@@ -15,9 +14,13 @@ const navigation = [
   { name: "Post Update", to: "/post"},
 ];
 
+const navigation_button_logout = [
+  { name: "Log out", to: "/"},
+]
+
 const navigation_button = [
-  { name: "Login", to: "/login"},
   { name: "Sign up", to: "/signup"},
+  { name: "Login", to: "/login"},
 ]
 
 const navigation_dropdown = [
@@ -42,11 +45,13 @@ function NavBar() {
     navigate("/login")
   }
 
+  console.log(user)
+
   return (
-    (
       <Disclosure as="nav" className="bg-white shadow">
         {({ open }) => (
           <>
+            {/* Logo */}
             <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
               <div className="flex justify-between h-16">
                 <div className="flex px-2 lg:px-0">
@@ -62,6 +67,8 @@ function NavBar() {
                       alt="Workflow"
                     />
                   </div>
+
+                  {/* navigation main pages */}
                   <div className="hidden lg:ml-6 lg:flex lg:space-x-8">
                   {navigation.map((item) => (
                       <Link
@@ -78,25 +85,8 @@ function NavBar() {
                     ))}                    
                   </div>
                 </div>
-                <div className="flex-1 flex items-center justify-center px-2 lg:ml-6 lg:justify-end">
-                  <div className="max-w-lg w-full lg:max-w-xs">
-                    <label htmlFor="search" className="sr-only">
-                      Search
-                    </label>
-                    <div className="relative">
-                      <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                        <SearchIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                      </div>
-                      <input
-                        id="search"
-                        name="search"
-                        className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        placeholder="Search"
-                        type="search"
-                      />
-                    </div>
-                  </div>
-                </div>
+
+                {/* Resize Menu */}
                 <div className="flex items-center lg:hidden">
                   {/* Mobile menu button */}
                   <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -108,28 +98,46 @@ function NavBar() {
                     )}
                   </Disclosure.Button>
                 </div>
+                
+                {/* login sign up button */}
                 <div className="hidden lg:ml-4 lg:flex lg:items-center">
-                <div className="flex-shrink-0">
-                {navigation_button.map((item) => (
-                  <Link key={item.name} to={item.to}>
-                  <button
-                    type="button"
-                    className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 m-3"
-                  >
-                    <span>{item.name}</span>
-                  </button>
-                  </Link>
-                ))}   
-                </div>
+                  {user?.["username"] !== undefined ? (
+                  <div className="flex-shrink-0">
+                    {navigation_button_logout.map((item) => (
+                      <Link key={item.name} to={item.to}>
+                      <button onClick={handleLogOut}
+                        type="button"
+                        className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 m-3"
+                      >
+                        <span>{item.name}</span>
+                      </button>
+                      </Link>
+                    ))}   
+                  </div>) : (
+                  <div className="flex-shrink-0">
+                    {navigation_button.map((item) => (
+                      <Link key={item.name} to={item.to}>
+                      <button
+                        type="button"
+                        className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 m-3"
+                      >
+                        <span>{item.name}</span>
+                      </button>
+                      </Link>
+                    ))}   
+                  </div>
+                  )}
+                
                 
                   {/* Profile dropdown */}
+                  {user?.["username"] !== undefined ? (
                   <Menu as="div" className="ml-4 relative flex-shrink-0">
                     <div>
                       <Menu.Button className="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         <span className="sr-only">Open user menu</span>
                         <img
                           className="h-8 w-8 rounded-full"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                          src={user?.["display_pic_url"]}
                           alt=""
                         />
                       </Menu.Button>
@@ -158,39 +166,41 @@ function NavBar() {
                       </Menu.Items>
                     </Transition>
                   </Menu>
+                  ) : null}
                 </div>
               </div>
             </div>
   
             <Disclosure.Panel className="lg:hidden">
               <div className="pt-2 pb-3 space-y-1">
+                {navigation.map((item) => (
+                  <Link
+                  key={item.name} to={item.to}>
                 <Disclosure.Button
-                  className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-                >
-                  Dashboard
-                </Disclosure.Button>
-                <Disclosure.Button
-                  as="a"
-                  href="#"
                   className="border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
                 >
-                  Team
+                  {item.name}
                 </Disclosure.Button>
+                </Link>
+                ))}
               </div>
+              
+              {user?.["username"] !== undefined ? (
               <div className="pt-4 pb-3 border-t border-gray-200">
                 <div className="flex items-center px-4">
                   <div className="flex-shrink-0">
                     <img
                       className="h-10 w-10 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      src={user?.["display_pic_url"]}
                       alt=""
                     />
                   </div>
                   <div className="ml-3">
-                    <div className="text-base font-medium text-gray-800">Tom Cook</div>
-                    <div className="text-sm font-medium text-gray-500">tom@example.com</div>
+                    <div className="text-base font-medium text-gray-800">{user?.username}</div>
+                    <div className="text-sm font-medium text-gray-500">{user?.email}</div>
                   </div>
                 </div>
+
                 <div className="mt-3 space-y-1">
                 {navigation_dropdown.map((item) => (
                 <Link key={item.name} to={item.to}>
@@ -203,34 +213,13 @@ function NavBar() {
                 ))}
                 </div>
               </div>
+              ) : null}
+              
             </Disclosure.Panel>
           </>
         )}
       </Disclosure>
-    )
-    // <div className="nav">
-
-    //   <Link to="/">Home</Link>
-    //   <Link to="/sell">Sell Item</Link>
-    //   <Link to="/feed">Feed</Link>
-    //   <Link to="/post">Post/Update</Link>
-
-    //   {user?.["username"] !== undefined ? (
-    //     <>
-    //       <Link to="/mysales">My Sales</Link>
-    //       <Link to="/mypurchase">My Purchase</Link>
-    //       <Link to="/settings">Settings</Link>
-    //     </>
-    //   ) : null}
-
-    //   {user?.["username"] !== undefined ? (
-    //     <button onClick={handleLogOut}>Log Out</button>
-        
-    //   ) : (
-    //     <Link to="/login">Sign up/ Login</Link>
-    //   )}
-    // </div>
-  );
+    );
 }
 
 export default NavBar;
