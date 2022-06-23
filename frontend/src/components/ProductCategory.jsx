993d237c94c6a54af808d6b40d31f3598911bc5d
limjiehao <jiehao15@gmail.com>
2022-06-23 15:56:57 +0800

    removed comments from product category page

--- frontend/src/components/ProductCategory.jsx
+++ frontend/src/components/ProductCategory.jsx
@@ -34,19 +34,6 @@ const ProductCategory = ({productdata}) => {
         </div>
       </div>
     </div>
-
-    // <div>
-    //     {productdata.map((element) => (
-    //     <div key={element._id}>
-    //         <p>{element.product_name}</p>
-    //         <img src={element.product_image} />
-    //         <p>{element.product_description}</p>
-    //         <p>{element.product_price_one_off}</p>
-    //         <p>{element.product_price_subscription}</p>
-    //         <p>{element.product_listed_date}</p>            
-    //     </div>
-    //   ))}
-    // </div>
   );
 };
 
