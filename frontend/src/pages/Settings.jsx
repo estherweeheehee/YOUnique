2c0a776450ead4fd41d3a86f8f596d70141ded3b
limjiehao <jiehao15@gmail.com>
2022-06-23 12:24:22 +0800

    settings updated.

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -66,7 +66,7 @@ const Settings = () => {
   };
 
   return (
-    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-6 sm:px-3 lg:max-w-7xl lg:px-8">
+    <div className="max-w-2xl mx-auto py-8 px-4 sm:py-4 sm:px-3 lg:max-w-7xl lg:px-8">
       {/* Description list with inline editing */}
       <div className="mt-10 divide-y divide-gray-200">
         <div className="space-y-1">
