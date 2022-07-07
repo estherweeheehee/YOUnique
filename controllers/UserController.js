4b35482fa1dded6d00206896b45f327d46f5d211
limjiehao <jiehao15@gmail.com>
2022-07-07 21:47:09 +0800

    Revert "jwt done"
    This reverts commit 40184bce8f85715af03d199e832a4359e79b02c8.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt");
 const { hashSync } = require("bcrypt");
 // const userdata = require("../data/userdata");
 
+
 const router = express.Router();
 const saltRounds = bcrypt.genSaltSync(10);
 
@@ -12,57 +13,53 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/seed", async (req, res) => {
-  try {
-    await User.deleteMany({});
-    const newUsers = await User.create([
-      {
-        username: "esther",
-        password: bcrypt.hashSync("123", saltRounds),
-        email: "esther@mail.com",
-        first_name: "Esther",
-        last_name: "Wee",
-        display_pic_url:
-          "https://media-exp2.licdn.com/dms/image/C5103AQEznnA5w3f52g/profile-displayphoto-shrink_800_800/0/1517506416229?e=1660780800&v=beta&t=P1klipOPIFA9JWPzCCX0n_GW7UH8FfWQmUfNhRYIqmw",
-        user_description:
-          "hello world i'm esther and I like durians. I sell cookies",
-      },
-      {
-        username: "lucas",
-        password: bcrypt.hashSync("123", saltRounds),
-        email: "lucas@mail.com",
-        first_name: "Lucas",
-        last_name: "Scott",
-        display_pic_url:
-          "https://scontent.fsin7-1.fna.fbcdn.net/v/t1.6435-9/72901431_10206357000560969_6209118440393801728_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=aSFlRyEVB_sAX-O93ET&tn=NEbFg-ajeMcLo6EP&_nc_ht=scontent.fsin7-1.fna&oh=00_AT_BYPlIWAb7hjC2r-2nnRHrIRyv84ry4mxR7d0TIGpTBA&oe=62D35181",
-        user_description: "hello world i'm lucas",
-      },
-      {
-        username: "jiehao",
-        password: bcrypt.hashSync("123", saltRounds),
-        email: "jiehao@mail.com",
-        first_name: "Jie",
-        last_name: "Hao",
-        display_pic_url:
-          "https://i.pinimg.com/originals/a7/4c/11/a74c11686bebb2bf37027473e0d595a3.jpg",
-        user_description: "hello world i'm jiehao",
-      },
-      {
-        username: "hello",
-        password: bcrypt.hashSync("world", saltRounds),
-        email: "hello@mail.com",
-        first_name: "jimmy",
-        last_name: "neutron",
-        display_pic_url:
-          "https://images.generated.photos/-NEf8sq8XZhVzzmu88RD_AnEODxf7KzV5_dvbNK3paM/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LmNvbmQvNjViMTli/MGItN2UwYy00NmFk/LWJjOGEtYmY5Nzgy/YTA2MjA4LmpwZw.jpg",
-        user_description:
-          "hello world i'm Jimmy Neutron and I have a big brain",
-      },
-    ]);
-    res.send(newUsers);
-  } catch (error) {
-    res.send(error);
-  }
-});
+      try {
+        await User.deleteMany({});
+        const newUsers = await User.create([
+            {
+                username: "esther",
+                password: bcrypt.hashSync("123", saltRounds),
+                email: "esther@mail.com",
+                first_name: "Esther",
+                last_name: "Wee",
+                display_pic_url: "https://media-exp2.licdn.com/dms/image/C5103AQEznnA5w3f52g/profile-displayphoto-shrink_800_800/0/1517506416229?e=1660780800&v=beta&t=P1klipOPIFA9JWPzCCX0n_GW7UH8FfWQmUfNhRYIqmw",
+                user_description: "hello world i'm esther and I like durians. I sell cookies"
+            },
+            {
+                username: "lucas",
+                password: bcrypt.hashSync("123", saltRounds),
+                email: "lucas@mail.com",
+                first_name: "Lucas",
+                last_name: "Scott",
+                display_pic_url: "https://scontent.fsin7-1.fna.fbcdn.net/v/t1.6435-9/72901431_10206357000560969_6209118440393801728_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=aSFlRyEVB_sAX-O93ET&tn=NEbFg-ajeMcLo6EP&_nc_ht=scontent.fsin7-1.fna&oh=00_AT_BYPlIWAb7hjC2r-2nnRHrIRyv84ry4mxR7d0TIGpTBA&oe=62D35181",
+                user_description: "hello world i'm lucas",
+            },
+            {
+                username: "jiehao",
+                password: bcrypt.hashSync("123", saltRounds),
+                email: "jiehao@mail.com",
+                first_name: "Jie",
+                last_name: "Hao",
+                display_pic_url: "https://i.pinimg.com/originals/a7/4c/11/a74c11686bebb2bf37027473e0d595a3.jpg",
+                user_description: "hello world i'm jiehao"
+            },
+            {
+                username: "hello",
+                password: bcrypt.hashSync("world", saltRounds),
+                email: "hello@mail.com",
+                first_name: "jimmy",
+                last_name: "neutron",
+                display_pic_url: "https://images.generated.photos/-NEf8sq8XZhVzzmu88RD_AnEODxf7KzV5_dvbNK3paM/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LmNvbmQvNjViMTli/MGItN2UwYy00NmFk/LWJjOGEtYmY5Nzgy/YTA2MjA4LmpwZw.jpg",
+                user_description: "hello world i'm Jimmy Neutron and I have a big brain"
+            }
+        ]);
+        res.send(newUsers)
+      } catch (error) {
+        res.send(error);
+      }
+    });
+    
+
 
 router.post("/signup", async (req, res) => {
   try {
@@ -152,44 +149,36 @@ router.get("/mypurchase/MS/:id", async (req, res) => {
 });
 
 router.put("/OForderstatus/:id", async (req, res) => {
-  const { id } = req.params;
-  if (!req.session.username) {
-    res.send({ status: "fail", data: "No access" });
-  } else {
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        id,
-        { $set: { sales_order_one_off: req.body } },
-        {
-          new: true,
+    const { id } = req.params;
+    if (!req.session.username) {
+        res.send({ status: "fail", data: "No access" });
+    } else {
+        try {
+            const updatedUser = await User.findByIdAndUpdate(id, { $set: {sales_order_one_off: req.body}}, {
+                new: true,
+              })
+            res.send(updatedUser)
+        } catch (error) {
+            res.send({ error: error });
         }
-      );
-      res.send(updatedUser);
-    } catch (error) {
-      res.send({ error: error });
     }
-  }
-});
+})
 
 router.put("/MSorderstatus/:id", async (req, res) => {
-  const { id } = req.params;
-  if (!req.session.username) {
-    res.send({ status: "fail", data: "No access" });
-  } else {
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        id,
-        { $set: { sales_order_subscription: req.body } },
-        {
-          new: true,
+    const { id } = req.params;
+    if (!req.session.username) {
+        res.send({ status: "fail", data: "No access" });
+    } else {
+        try {
+            const updatedUser = await User.findByIdAndUpdate(id, { $set: {sales_order_subscription: req.body}}, {
+                new: true,
+              })
+            res.send(updatedUser)
+        } catch (error) {
+            res.send({ error: error });
         }
-      );
-      res.send(updatedUser);
-    } catch (error) {
-      res.send({ error: error });
     }
-  }
-});
+})
 
 router.put("/settings/:id", async (req, res) => {
   const { id } = req.params;
@@ -217,6 +206,7 @@ router.delete("/settings/:id", async (req, res) => {
       req.session.destroy();
       res.send(deletedUser);
     } catch (error) {
+      
       res.send({ error: error });
     }
   }
@@ -286,20 +276,20 @@ router.get("/feed/:id", async (req, res) => {
 });
 
 router.get("/search/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const users = await User.find();
-    const result = [];
-    for (let element of users) {
-      if (element.username.toLowerCase().includes(id)) {
-        result.push(element);
+    const { id } = req.params;
+    try {
+        const users = await User.find()
+      const result = [];
+      for (let element of users) {
+        if (element.username.toLowerCase().includes(id) ) {
+          result.push(element)
+        }
       }
-    }
-
-    res.send(result);
-  } catch (error) {
+      
+      res.send(result);
+    } catch (error) {
     res.send({ error: error });
-  }
-});
+    }
+  });
 
 module.exports = router;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -27,10 +27,9 @@ const Signup = () => {
   const useDefault = () => {
     setSignUp({
       ...signUp,
-      display_pic_url:
-        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
-    });
-  };
+      display_pic_url: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
+    })
+  }
 
   const handleSubmitSignup = (event) => {
     event.preventDefault();
@@ -50,7 +49,7 @@ const Signup = () => {
           navigate("/login");
         } else {
           setUser(data);
-          console.log(data);
+          console.log(data)
           navigate("/");
         }
       });
@@ -58,7 +57,7 @@ const Signup = () => {
 
   return (
     <>
-      {/* logo plus wording */}
+    {/* logo plus wording */}
       <div className="min-h-full flex flex-col justify-center py-8 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <img
@@ -66,26 +65,16 @@ const Signup = () => {
             src="https://i.imgur.com/4fZaiaq.png"
             alt="Workflow"
           />
-          <h2 className="mt-4 text-center text-3xl font-extrabold text-gray-900">
-            Sign up a new account
-          </h2>
+          <h2 className="mt-4 text-center text-3xl font-extrabold text-gray-900">Sign up a new account</h2>
         </div>
 
         {/* user name & password */}
         <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form
-              className="space-y-6"
-              action="#"
-              method="POST"
-              onSubmit={handleSubmitSignup}
-            >
-              <div className="grid grid-cols-6 gap-6">
+            <form className="space-y-6" action="#" method="POST" onSubmit={handleSubmitSignup}>
+            <div className="grid grid-cols-6 gap-6">
                 <div className="col-span-6 sm:col-span-3">
-                  <label
-                    htmlFor="username"
-                    className="block text-sm font-medium text-gray-700"
-                  >
+                  <label htmlFor="username" className="block text-sm font-medium text-gray-700">
                     Username
                   </label>
                   <input
@@ -99,10 +88,7 @@ const Signup = () => {
                 </div>
 
                 <div className="col-span-6 sm:col-span-3">
-                  <label
-                    htmlFor="password"
-                    className="block text-sm font-medium text-gray-700"
-                  >
+                  <label htmlFor="password" className="block text-sm font-medium text-gray-700">
                     Password
                   </label>
                   <input
@@ -118,10 +104,7 @@ const Signup = () => {
 
               {/* email */}
               <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-700"
-                >
+                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                   Email address
                 </label>
                 <div className="mt-1">
@@ -136,14 +119,11 @@ const Signup = () => {
                   />
                 </div>
               </div>
-
+              
               {/* Name */}
               <div className="grid grid-cols-6 gap-6">
                 <div className="col-span-6 sm:col-span-3">
-                  <label
-                    htmlFor="first-name"
-                    className="block text-sm font-medium text-gray-700"
-                  >
+                  <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
                     First name
                   </label>
                   <input
@@ -158,10 +138,7 @@ const Signup = () => {
                 </div>
 
                 <div className="col-span-6 sm:col-span-3">
-                  <label
-                    htmlFor="last-name"
-                    className="block text-sm font-medium text-gray-700"
-                  >
+                  <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
                     Last name
                   </label>
                   <input
@@ -178,19 +155,11 @@ const Signup = () => {
 
               {/* display picture */}
               <div>
-                <label
-                  htmlFor="userpicture"
-                  className="flex text-sm font-medium text-gray-700"
-                >
+                <label htmlFor="userpicture" className="flex text-sm font-medium text-gray-700">
                   Display picture URL
-                  <p
-                    className="mx-2 mb-1 text-xs text-white font-medium bg-indigo-600 px-2 py-1 w-max rounded-md"
-                    onClick={useDefault}
-                  >
-                    Click to use default image
-                  </p>
+                  <p className="mx-2 mb-1 text-xs text-white font-medium bg-indigo-600 px-2 py-1 w-max rounded-md" onClick={useDefault}>Click to use default image</p>
                 </label>
-
+                
                 <div className="mt-1">
                   <input
                     id="userpicture"
@@ -202,14 +171,12 @@ const Signup = () => {
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   />
                 </div>
+                
               </div>
 
               {/* display picture */}
               <div>
-                <label
-                  htmlFor="profiledescription"
-                  className="block text-sm font-medium text-gray-700"
-                >
+                <label htmlFor="profiledescription" className="block text-sm font-medium text-gray-700">
                   Profile Description
                 </label>
                 <div className="mt-1">
